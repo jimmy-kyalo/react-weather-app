010aa174ca189ba82994d028486f280e472b6a75
jimmy-kyalo <kyalo90@gmail.com>
2022-07-15 19:05:27 +0300

    api call

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4dfea70568msha602f4a89991846p14a981jsn495b2abc7b64",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "549670c1081dac7ae38a394924cecaa8";
